711d7965bb1f23bc7d7ff0d584170fb71a8e30c7
Infroid <86107234+Infroid-Coder@users.noreply.github.com>
2022-02-22 12:02:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let something = "";
+
+function play() {
+    document.body.style.animationName = "bgplay";
+    document.body.style.animationDuration = "3s"
+    document.body.style.animationIterationCount = "infinite";
+    document.body.style.animationTimingFunction = "linear";
+}
